0ef459dec4d3f5cff556ccb1927966758c06d528
atvKail <mrxyzi2007@gmail.com>
2025-12-12 05:28:20 +0500

    upd

--- Practice12.12.2025/1.cpp
+++ Practice12.12.2025/1.cpp
@@ -15,17 +15,41 @@ void print_matrix(shi **matrix) {
     }
 }
 
+// int main() {
+//     unsigned int n;
+//     cin >> n;
+
+//     shi **T = new shi*[n];
+//     forn(i, n) T[i] = new shi[n];
+
+//     forn(i, n)
+//         forn(j, n)
+//             T[i][j] = ((i + j) % 2 == (n - 1) % 2) ? 0 : 1;
+//             // T[i][j] = !((i + j) % 2 == (n - 1) % 2);
+//     print_matrix(T);
+
+//     delete[] T;
+//     return 0;
+// }
+
 int main() {
     unsigned int n;
     cin >> n;
 
+    shi *data = new shi[ (size_t)n * n ];
     shi **T = new shi*[n];
-    forn(i, n) T[i] = new shi[n];
-
-    forn(i, n)
-        forn(j, n)
-            T[i][j] = ((i + j) % 2 == (n - 1) % 2) ? 0 : 1;
-            // T[i][j] = !((i + j) % 2 == (n - 1) % 2);
+    for (unsigned int i = 0; i < n; ++i)
+        *(T + i) = data + (size_t)i * n;
+    
+    for (unsigned int i = 0; i < n; ++i) {
+        shi *row = *(T + i);
+        for (unsigned int j = 0; j < n; ++j)
+            *(row + j) = !((i + j) % 2 == (n - 1) % 2);
+    }
     
     print_matrix(T);
+
+    delete[] data;
+    delete[] T;
+    return 0;
 }
