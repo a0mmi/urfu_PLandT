6079cd386a20bdcb059130a3cf9323da24a4b12f
atvKail <mrxyzi2007@gmail.com>
2026-04-15 11:51:44 +0500

    upd

--- second half-year of the 1st course/Practice15.04.2026/main.cpp
+++ second half-year of the 1st course/Practice15.04.2026/main.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 
 using db = double;
-using ldb = long db;
-
-#define ПРАВОСЛАВНЫЙ_ЦИКЛ(i, st, en) for(int i = st; i < en; ++i)
+using ldb = long double;
 
 ldb f1(ldb x) {
     return x * x * x;
@@ -18,11 +16,11 @@ ldb f3(ldb x) {
     return 1.0L / x;
 }
 
-ldb Integral(ldb (*f)(ldb), ldb a, ldb b, int n) { // точность n
+ldb integral(ldb (*f)(ldb), ldb a, ldb b, int n) {
     ldb h = (b - a) / n;
     ldb sum = 0.0L;
 
-    ПРАВОСЛАВНЫЙ_ЦИКЛ(i, 0, n) {
+    for (int i = 0; i < n; ++i) {
         ldb x = a + (i + 0.5L) * h;
         sum += f(x);
     }
@@ -32,12 +30,13 @@ ldb Integral(ldb (*f)(ldb), ldb a, ldb b, int n) { // точность n
 
 int main() {
     ldb a, b;
-    int type, n;
+    int n;
 
     cin >> a >> b >> n;
 
-    cout << Integral(f1, a, b, n) << '\n';
-    cout << Integral(f2, a, b, n) << '\n';
-    cout << Integral(f3, a, b, n) << '\n';
+    cout << integral(f1, a, b, n) << '\n';
+    cout << integral(f2, a, b, n) << '\n';
+    cout << integral(f3, a, b, n) << '\n';
+
     return 0;
 }
